3cd52a9ef4aadac6d3890a18f98d5c5257877b0a
midou123 <cui_no2013@163.com>
2015-12-18 20:22:29 +0800

    a test

---
+++ app/src/main/java/com/example/opt/gittest/Test.java
@@ -0,0 +1,52 @@
+package com.example.opt.gittest;
+
+/**
+ * Created by opt on 2015/12/18.
+ */
+public class Test {
+    private int age;
+    private int mobile;
+    private String name;
+    private String address;
+    private String email;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(int mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
