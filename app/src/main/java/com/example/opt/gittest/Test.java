593a80e93043dc04967ca1c6042a6b53a1ca21cb
midou123 <cui_no2013@163.com>
2015-12-18 20:40:18 +0800

    a test

--- app/src/main/java/com/example/opt/gittest/Test.java
+++ app/src/main/java/com/example/opt/gittest/Test.java
@@ -10,13 +10,31 @@ public class Test {
     private String address;
     private String email;
 
+    private int height;
+    private int weight;
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
     public int getAge() {
         return age;
     }
 
     public void setAge(int age) {
         this.age = age;
-        /***/
     }
 
     public int getMobile() {
